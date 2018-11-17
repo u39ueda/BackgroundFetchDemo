f7b58f4c874c439abcf29d5ba84857ac437d582d
u39ueda <ueda.u39@gmail.com>
2018-11-17 11:06:03 +0900

    Create project

---
+++ BackgroundFetchDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundFetchDemo
+//
+//  Created by 植田裕作 on 2018/11/17.
+//  Copyright © 2018 Yusaku Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
